05652a37b92fa7b19a3268a3408c064a4c07ce94
AlexQuinn--SDE <1527962551@qq.com>
2026-08-09 21:24:17 +0800

    Add ProductController for managing products

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
