3590c55add96f1d2104975d63fb3f13d158d449d
AlexQuinn--SDE <1527962551@qq.com>
2026-08-10 15:56:52 +0800

    Fix formatting by removing extra newline
    Removed unnecessary newline at the end of the file.

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -12,7 +12,6 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     @Id
