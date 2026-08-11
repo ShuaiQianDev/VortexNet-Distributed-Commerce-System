c1b93880cc4096cc06cf191fe323f1edd9756e13
AlexQuinn--SDE <1527962551@qq.com>
2026-08-11 21:25:22 +0800

    Create InventoryRepository interface for inventory access

---
+++ src/main/java/com/example/Repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Inventory findByProductId(Long productId);
+}
